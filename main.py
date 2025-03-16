712e0b54fef042a97beac2dd523be6399cb58675
Maria Amorocho <m.amorocho@uniandes.edu.co>
2025-03-16 16:34:45 -0500

    arreglo main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return FileResponse("index.html")
+
+if __name__ == "__main__":
+    # Configuración para permitir acceso externo
+    uvicorn.run(app, host="0.0.0.0", port=8080)
